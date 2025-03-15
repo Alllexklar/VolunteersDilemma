4050a09ce1985c7de075aee9963aee2b25141adf
Jeff <stfu@stfu.uk>
2025-03-15 22:50:16 +0100

    idk a lot happend

---
+++ test1/test1/__init__.py
@@ -0,0 +1,42 @@
+from otree.api import *
+
+
+doc = """
+Your app description
+"""
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'test1'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    satisfaction = models.IntegerField(
+        label="How satisfied are you with the experiment?",
+        choices=[
+            [1, 'Strongly Disagree'],
+            [2, 'Disagree'],
+            [3, 'Neutral'],
+            [4, 'Agree'],
+            [5, 'Strongly Agree']
+        ],
+        widget=widgets.RadioSelectHorizontal
+    )
+
+
+# PAGES
+class Questionnaire(Page):
+    form_model = 'player'
+    form_fields = ['satisfaction']
+
+page_sequence = [Questionnaire]
--- volunteersdilemma1/settings.py
+++ volunteersdilemma1/settings.py
@@ -1,13 +1,12 @@
 from os import environ
 
-'''SESSION_CONFIGS = [
-    dict(
-        name='volunteersdilemma',
-        display_name="Questionnaire Experiment",
-        num_demo_participants=3,
-        app_sequence=['volunteersdilemma'],
-    ),
-]'''
+SESSION_CONFIGS = [
+    # dict(
+    #     name='public_goods',
+    #     app_sequence=['public_goods'],
+    #     num_demo_participants=3,
+    # ),
+]
 
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
 # in SESSION_CONFIGS, except those that explicitly override it.
@@ -35,4 +34,4 @@ ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 
 DEMO_PAGE_INTRO_HTML = """ """
 
-SECRET_KEY = '7216932154119'
+SECRET_KEY = '7292570324700'
